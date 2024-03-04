c859091bf26142a6bd5ff134588d207b7ffe5151
xhyuaner <xhyuaner@gmail.com>
2024-03-04 20:29:09 +0800

    feat: add algorithm

---
+++ 8-67jianzhi-offer/18-螺旋矩阵.go
@@ -0,0 +1,29 @@
+package main
+
+func spiralOrder(matrix [][]int) (result []int) {
+	top, left := 0, 0
+	bottom, right := len(matrix)-1, len(matrix[0])-1
+	for left <= right && top <= bottom {
+		direction := left
+		for ; direction <= right; direction++ {
+			result = append(result, matrix[top][direction])
+		}
+		direction = top + 1
+		for ; direction <= bottom; direction++ {
+			result = append(result, matrix[direction][right])
+		}
+		if left < right && top < bottom {
+			direction = right - 1
+			for ; direction >= left; direction-- {
+				result = append(result, matrix[bottom][direction])
+			}
+			direction = bottom - 1
+			for ; direction > top; direction-- {
+				result = append(result, matrix[direction][left])
+			}
+		}
+		top, left = top+1, left+1
+		bottom, right = bottom-1, right-1
+	}
+	return result
+}
