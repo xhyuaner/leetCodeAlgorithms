c14a1a082067a3b2a296cd91348c0a33324756d7
xhyuaner <xhyuaner@gmail.com>
2024-03-20 21:33:08 +0800

    feat: add algorithm

--- 8-67jianzhi-offer/35-删除排序链表中重复元素.go
+++ 8-67jianzhi-offer/35-删除排序链表中重复元素.go
@@ -4,15 +4,18 @@ func deleteDuplicates(head *ListNode) *ListNode {
 	if head == nil || head.Next == nil {
 		return head
 	}
+	// 指定 当前结点 和 它的前一个结点
 	pre, cur := head, head.Next
 	for cur != nil {
+		// 如果当前结点和前一个结点相同，就让当前结点一直++，直到遇到与pre结点不同的结点
 		if cur.Val == pre.Val {
 			cur = cur.Next
-		} else {
+		} else { // 将前面的重复结点一次性删除，让pre=cur，但是cur此时不用++,因为下次循环会++
 			pre.Next = cur
 			pre = cur
 		}
 	}
+	// 将pre结点的后一个结点置空
 	pre.Next = cur
 	return head
 }
---
+++ 8-67jianzhi-offer/36-输出数组中出现频率最高的K个数.go
@@ -0,0 +1,40 @@
+package jianzhi
+
+import "container/heap"
+
+type Iheap [][2]int
+
+func (h Iheap) Len() int { return len(h) }
+func (h Iheap) Less(i, j int) bool {
+	return h[i][1] < h[j][1]
+}
+func (h Iheap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+func (h *Iheap) Push(x interface{}) {
+	*h = append(*h, x.([2]int))
+}
+func (h *Iheap) Pop() interface{} {
+	x := (*h)[len(*h)-1]
+	*h = (*h)[:len(*h)-1]
+	return x
+}
+func topKFrequent(nums []int, k int) []int {
+	mp := map[int]int{}
+	for _, v := range nums {
+		mp[v]++
+	}
+	h := &Iheap{}
+	heap.Init(h)
+	for key, value := range mp {
+		heap.Push(h, [2]int{key, value})
+		if h.Len() > k {
+			heap.Pop(h)
+		}
+	}
+	res := make([]int, k)
+	for i := k; i > 0; i-- {
+		res[i-1] = heap.Pop(h).([2]int)[0]
+	}
+	return res
+}
