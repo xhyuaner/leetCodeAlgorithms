45ce9b1903a16d6d1ce3a792ebd8709b8b6acea0
xhyuaner <xhyuaner@gmail.com>
2024-03-21 16:56:07 +0800

    feat: add algorithm

---
+++ 8-67jianzhi-offer/37-合并区间.go
@@ -0,0 +1,18 @@
+package jianzhi
+
+import "sort"
+
+func merge(intervals [][]int) [][]int {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+	merged := make([][]int, 0)
+	for _, interval := range intervals {
+		if len(merged) == 0 || merged[len(merged)-1][1] < interval[0] {
+			merged = append(merged, interval)
+		} else {
+			merged[len(merged)-1][1] = max(merged[len(merged)-1][1], interval[1])
+		}
+	}
+	return merged
+}
