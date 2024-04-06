7b93083e27efde6f1d5f0af1d4982643e8fcc77f
xhyuaner <xhyuaner@gmail.com>
2024-04-06 19:26:31 +0800

    feat: add algorithm

--- 8-67jianzhi-offer/4-根据前中序推理二叉树.go
+++ 8-67jianzhi-offer/4-根据前中序推理二叉树.go
@@ -6,10 +6,6 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-// 使用map存储中序遍历列表，加速树节点Val的查找时间
-var inorderMap map[int]int
-var preord []int
-
 /**
  * deduceTree
  *  @Description:
@@ -18,32 +14,24 @@ var preord []int
  *  @return *TreeNode
  */
 func deduceTree(preorder []int, inorder []int) *TreeNode {
-	preord = preorder
-	inorderMap = make(map[int]int, len(inorder))
+	inorderMap := make(map[int]int, len(inorder))
 	// 将中序遍历列表存入map中
+	// 使用map存储中序遍历列表，加速树节点Val的查找时间
 	for i, v := range inorder {
 		inorderMap[v] = i
 	}
+	var recur func(rootIndex, left, right int) *TreeNode
+	recur = func(rootIndex, left, right int) *TreeNode {
+		if left > right {
+			return nil
+		}
+		root := preorder[rootIndex]
+		newTree := &TreeNode{Val: root}
+		i := inorderMap[root]
+		newTree.Left = recur(rootIndex+1, left, i-1)
+		newTree.Right = recur(rootIndex+(i-left)+1, i+1, right)
+		return newTree
+	}
 	// 返回递归子树
 	return recur(0, 0, len(inorder)-1)
-}
-
-/**
- * recur
- *  @Description:递归函数
- *  @param rootIndex
- *  @param left
- *  @param right
- *  @return *TreeNode
- */
-func recur(rootIndex, left, right int) *TreeNode {
-	if left > right {
-		return nil
-	}
-	root := preord[rootIndex]
-	newTree := &TreeNode{Val: root}
-	i := inorderMap[root]
-	newTree.Left = recur(rootIndex+1, left, i-1)
-	newTree.Right = recur(rootIndex+i-left+1, i+1, right)
-	return newTree
 }
