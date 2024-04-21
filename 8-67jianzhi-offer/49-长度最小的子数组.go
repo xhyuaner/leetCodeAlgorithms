299fca6b3b7c643d4003ab6aa20917f36f1c562b
xhyuaner <xhyuaner@gmail.com>
2024-04-21 19:31:16 +0800

    feat: add algorithm

---
+++ 8-67jianzhi-offer/49-长度最小的子数组.go
@@ -0,0 +1,30 @@
+package jianzhi
+
+/**
+ * minSubArrayLen（滑动窗口）
+ *  @Description: 给定一个含有 n 个正整数的数组和一个正整数 target 。
+				找出该数组中满足其总和大于等于 target 的长度最小的 连续子数组
+ 				[numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
+ *  @param target
+ *  @param nums
+ *  @return int
+*/
+func minSubArrayLen(target int, nums []int) int {
+	n := len(nums)
+	sum, left := 0, 0
+	ans := n + 1
+	for right := 0; right < n; right++ {
+		sum += nums[right]
+		for sum-nums[left] >= target {
+			sum -= nums[left]
+			left++
+		}
+		if sum >= target {
+			ans = min(ans, right-left+1)
+		}
+	}
+	if ans <= n {
+		return ans
+	}
+	return 0
+}
