f0eb2532bf34765d8c18a5b759628dfdd4e12bd3
xhyuaner <xhyuaner@gmail.com>
2024-04-21 20:04:06 +0800

    feat: add algorithm

--- 8-67jianzhi-offer/49-长度最小的子数组.go
+++ 8-67jianzhi-offer/49-长度最小的子数组.go
@@ -5,6 +5,9 @@ package jianzhi
  *  @Description: 给定一个含有 n 个正整数的数组和一个正整数 target 。
 				找出该数组中满足其总和大于等于 target 的长度最小的 连续子数组
  				[numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
+输入：target = 7, nums = [2,3,1,2,4,3]
+输出：2
+解释：子数组 [4,3] 是该条件下的长度最小的子数组。
  *  @param target
  *  @param nums
  *  @return int
