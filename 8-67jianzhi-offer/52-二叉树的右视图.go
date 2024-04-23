b17f137832cf0a4ce5dde8b7770981b95d576f42
xhyuaner <xhyuaner@gmail.com>
2024-04-23 21:11:41 +0800

    feat: add algorithm

---
+++ 8-67jianzhi-offer/52-二叉树的右视图.go
@@ -0,0 +1,23 @@
+package jianzhi
+
+/**
+ * rightSideView
+ *  @Description:给定一个二叉树的 根节点 root，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+ *  @param root
+ *  @return ans
+ */
+func rightSideView(root *TreeNode) (ans []int) {
+	var dfs func(*TreeNode, int)
+	dfs = func(node *TreeNode, depth int) {
+		if node == nil {
+			return
+		}
+		if depth == len(ans) {
+			ans = append(ans, node.Val)
+		}
+		dfs(node.Right, depth+1)
+		dfs(node.Left, depth+1)
+	}
+	dfs(root, 0)
+	return ans
+}
